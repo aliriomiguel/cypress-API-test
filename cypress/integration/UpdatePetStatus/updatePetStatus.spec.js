e6836f5d635622a45b49ce7a7f7bb5ee34c134e0
aliriomiguel <aliriomiguel@aranguren.us>
2023-04-03 16:23:31 +0200

    fixed update pet steps, added the correct POST call

--- cypress/integration/UpdatePetStatus/updatePetStatus.spec.js
+++ cypress/integration/UpdatePetStatus/updatePetStatus.spec.js
@@ -1,5 +1,5 @@
 Given('I update the pet with id {string} status to {string}', (petId, status) => {
-  cy.request('GET', "https://petstore.swagger.io/v2/pet/"+petId+"?status="+status).as('updatePet');
+  cy.request('POST', "https://petstore.swagger.io/v2/pet/"+petId+"?status="+status).as('updatePet');
 });
 
 Then('the response status code should be {int}', (statusCode) => {
